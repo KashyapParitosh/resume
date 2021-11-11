15ceb29762047eb158a3bce6ab2aea737d75026d
paritosh251197 <paritoshkashyap25@gmail.com>
2021-11-11 16:43:07 +0530

    sdf

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -2,10 +2,11 @@ import React from 'react';
 import "./Intro.css"
 import "./../../App.css"
 import leftSide from "../images/WhatsApp_Image_2021-11-08_at_12.01.06_PM-removebg-preview.png";
+import Resume from '../images/resume.pdf'
 export default function Intro() {
     return (
         <>
-            <div className="main-intro" translate="no">
+            <div className="main-intro" translate="no"> 
                 <div className="intro-flex">
                     <div className="intro-div">
                         <div className="name-intro">
@@ -18,7 +19,7 @@ export default function Intro() {
 
                         <p>Get ready to turn ideas into reality</p>
                         <div className="buttonshire">   <span className="hire-me-btn"> <a href="#Contact" >Hire Me</a></span>
-                            <span className="resume-btn"><a href="#Resume" >Get Resume</a></span> </div>
+                            <span className="resume-btn"><a href={Resume} download >Get Resume</a></span> </div>
                     </div>
                     <img className="hero-img" src={leftSide} alt="Header" />
                 </div>
