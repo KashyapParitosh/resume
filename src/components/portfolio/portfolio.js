337f1e845a119ccffabd46c93052fc22d3b11bd7
paritosh251197 <paritoshkashyap25@gmail.com>
2022-01-20 21:58:30 +0530

    changes doneT

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -11,13 +11,15 @@ class Portfolio extends Component{
                 <h2 className= "portfolioHeading">My Portfolio</h2>
                 <div className="underline"></div>
                 <div className="portfolio-img-container">
-                    <a href="https://paritosh251197.github.io/clock_project_js/" target="_blank" rel="noreferrer"><div className="item1 items"></div></a>
+                    <a href="https://react-paritosh-blog.netlify.app/" target="_blank" rel="noreferrer"><div className="item6 items"></div></a>
+
+                    <a href="https://kashyapparitosh.github.io/clock_project_js/" target="_blank" rel="noreferrer"><div className="item1 items"></div></a>
                     
-                    <a href="https://paritosh251197.github.io/todoJs/" target="_blank" rel="noreferrer"><div className="item2 items"></div></a>
+                    <a href="https://kashyapparitosh.github.io/todoJs/" target="_blank" rel="noreferrer"><div className="item2 items"></div></a>
                     
-                    <a href="https://paritosh251197.github.io/cssass6/" target="_blank" rel="noreferrer"><div className="item3 items"></div></a>
+                    <a href="https://kashyapparitosh.github.io/cssass6/" target="_blank" rel="noreferrer"><div className="item3 items"></div></a>
                     
-                    <a href="https://paritosh251197.github.io/cssass5/" target="_blank" rel="noreferrer"><div className="item4 items"></div></a>
+                    <a href="https://kashyapparitosh.github.io/cssass5/" target="_blank" rel="noreferrer"><div className="item4 items"></div></a>
                     
                     <a href="https://csb-1etn8.netlify.app/" target="_blank" rel="noreferrer"><div className="item5 items"></div></a>
                 </div>
